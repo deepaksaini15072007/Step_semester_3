d7296c5f041a01476b07e24e401b38818a8766d6
Deepak saini <deepaksaini15072007@gmail.com>
2026-08-23 18:54:08 +0530

    Add star pattern program

--- src/main/java/control_flow/class_problems/StarPattern.java
+++ src/main/java/control_flow/class_problems/StarPattern.java
@@ -1,5 +1,17 @@
 package control_flow.class_problems;
-
+import java.util.Scanner;
 public class StarPattern {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number of Rows: ");
+        int Rows = sc.nextInt();
+        for(int i = 1 ; i<=Rows;i++){
+            for(int j = 1; j<=i;j++){
+                System.err.print("* ");
+            }
+            System.err.println();
+        }
+
+    }    
+
 }
