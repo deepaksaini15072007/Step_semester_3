0c3470a66b19a2809e9430231b93cd5259b06c40
Deepak saini <deepaksaini15072007@gmail.com>
2026-08-23 18:57:59 +0530

    Add star pattern program

--- src/main/java/control_flow/class_problems/StarPattern.java
+++ src/main/java/control_flow/class_problems/StarPattern.java
@@ -4,8 +4,8 @@ public class StarPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number of Rows: ");
-        int Rows = sc.nextInt();
-        for(int i = 1 ; i<=Rows;i++){
+        int rows = sc.nextInt();
+        for(int i = 1 ; i<=rows;i++){
             for(int j = 1; j<=i;j++){
                 System.err.print("* ");
             }
