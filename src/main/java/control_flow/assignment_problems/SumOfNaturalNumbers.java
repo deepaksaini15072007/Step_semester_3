8db42ba791c3f89f435586be7aa80bf65753c8c8
Deepak saini <deepaksaini15072007@gmail.com>
2026-08-29 14:49:17 +0530

    Add sum of natural numbers using while loop

---
+++ src/main/java/control_flow/assignment_problems/SumOfNaturalNumbers.java
@@ -0,0 +1,25 @@
+package control_flow.assignment_problems;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        sc.close();
+    }
+}
