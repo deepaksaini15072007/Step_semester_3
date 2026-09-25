89c755c9c3a65e399619a05a8d804ba82b71fc30
Deepak saini <deepaksaini15072007@gmail.com>
2026-09-25 16:46:23 +0530

    Add Nickname Tag

---
+++ src/main/java/Week7Problem/class_problems/NameTag.java
@@ -0,0 +1,26 @@
+package Week7Problem.class_problems;
+
+public class NameTag {
+
+    private final String firstName;
+    private final char lastInitial;
+
+    public NameTag(String fullName) {
+
+        String[] parts = fullName.split(" ");
+
+        firstName = parts[0];
+        lastInitial = parts[1].charAt(0);
+    }
+
+    public String getNickname() {
+        return firstName + " " + lastInitial + ".";
+    }
+
+    public static void main(String[] args) {
+
+        NameTag tag = new NameTag("Maria Gomez");
+
+        System.out.println(tag.getNickname());
+    }
+}
