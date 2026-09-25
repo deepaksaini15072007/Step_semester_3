dbb13934d2d329cafe4a541c71f76ddf5fa68f2a
Deepak saini <deepaksaini15072007@gmail.com>
2026-09-25 15:08:48 +0530

    Add Employee and Company Information management

---
+++ src/main/java/Week6Problem/assignment_problems/EmployeeCompanyInformationManagement.java
@@ -0,0 +1,35 @@
+package Week6Problem.assignment_problems;
+
+public class EmployeeCompanyInformationManagement {
+
+    static class Employee {
+
+        String empName;
+        double salary;
+
+        static String companyName =
+                "Bright Horizon Technologies";
+
+        static int employeeCount = 0;
+
+        Employee(String empName, double salary) {
+            this.empName = empName;
+            this.salary = salary;
+            employeeCount++;
+        }
+
+        static void printCompanyInfo() {
+            System.out.println(companyName);
+            System.out.println("Employees on record: " + employeeCount);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Employee employee1 = new Employee("Divya", 65000);
+        Employee employee2 = new Employee("Arjun", 30000);
+        Employee employee3 = new Employee("Rahul", 50000);
+
+        Employee.printCompanyInfo();
+    }
+}
