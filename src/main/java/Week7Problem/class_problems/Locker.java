635166c28c219386d6b57f13cd44b3d6b4d714b1
Deepak saini <deepaksaini15072007@gmail.com>
2026-09-25 16:50:38 +0530

    Add Locker code

---
+++ src/main/java/Week7Problem/class_problems/Locker.java
@@ -0,0 +1,30 @@
+package Week7Problem.class_problems;
+
+public class Locker {
+
+    private final int lockerNumber;
+    private String combinationCode;
+
+    public Locker(int lockerNumber, String combinationCode) {
+        this.lockerNumber = lockerNumber;
+        this.combinationCode = combinationCode;
+    }
+
+    public void changeCode(String currentCode, String newCode) {
+
+        if (combinationCode.equals(currentCode)) {
+            combinationCode = newCode;
+            System.out.println("Code changed successfully");
+        } else {
+            System.out.println("Code change rejected");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Locker l = new Locker(101, "1234");
+
+        l.changeCode("1234", "5678");
+        l.changeCode("0000", "9999");
+    }
+}
